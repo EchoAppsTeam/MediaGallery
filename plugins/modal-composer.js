ec1dea19118c84aa92f2af3c33d136153fde31b2
Ilya Mikhailov <mikhailov@aboutecho.com>
2014-04-24 15:15:08 +0400

    modal composer polishing

--- plugins/modal-composer.js
+++ plugins/modal-composer.js
@@ -8,58 +8,59 @@ if (Echo.Plugin.isDefined(plugin)) return;
 plugin.templates.submitPanel =
 	'<div class="{plugin.class:submitPanel}">' +
 		'<div class="{plugin.class:submitHead}">' +
-			'<div class="{plugin.class:submitHeadBrand}">{plugin.label:submitHeadBrand}</div>' +
-			'<div class="{plugin.class:submitHeadText}">{plugin.label:submitHeadText}</div>' +
+			'<div class="{plugin.class:submitHeadBrand}">{plugin.label:title}</div>' +
+			'<div class="{plugin.class:submitHeadText}">{plugin.label:description}</div>' +
 		'</div>' +
-		'<button class="{plugin.class:submitButton} btn btn-primary btn-small"></button>' +
+		'<button class="{plugin.class:submitButton} btn btn-primary"></button>' +
 		'<div class="echo-clear"></div>' +
-	'</div>' +
-	'<div class="{class:postComposer}"></div>';
-
-plugin.labels = {
-	"submitHeadBrand": "Participate in our amazing video wall",
-	"submitHeadText": "Tweet, Instagram, or Facebook with #awesome tag or submit your photos right on the page!",
-	"submitButtonText": "Submit your media"
-};
+		'<div class="{class:postComposer}"></div>' +
+	'</div>';
 
 plugin.init = function() {
-	this.extendTemplate("replace", "postComposer", plugin.templates.submitPanel);
+	this.labels.set({
+		"title": this.config.get("nativeSubmissions.title"),
+		"description": this.config.get("nativeSubmissions.description"),
+		"buttonText": this.config.get("nativeSubmissions.buttonText")
+	});
+	if (this.config.get("nativeSubmissions.visible")) {
+		this.extendTemplate("replace", "postComposer", plugin.templates.submitPanel);
+	}
 };
 
 plugin.component.renderers.postComposer = function(element) {
 	this.component.parentRenderer("postComposer", arguments);
-	//element.hide();
-	return element;
+	new Echo.GUI.Modal({
+		"show": !!this.get("composerRendered"),
+		"data": {
+			"title": this.labels.get("buttonText"),
+			"body": element
+		}
+	});
+	this.set("composerRendered", true);
 };
 
+// TODO: think about composer and modal window reuseability
 plugin.renderers.submitButton = function(element) {
-	var component = this.component;
+	var self = this;
 	new Echo.GUI.Button({
 		"target": element,
 		"icon": false,
 		"disabled": false,
-		"label": this.labels.get("submitButtonText")
+		"label": this.labels.get("buttonText")
 	});
 	element.click(function() {
-		var composer = component.view.get("postComposer");
-		new Echo.GUI.Modal({
-			"show": true,
-			"data": {
-				"body": composer
-			}
-		});
-		composer.show();
+		self.component.render({"name": "postComposer"});
 	});
 	return element;
 };
 
 plugin.css =
-	'.{plugin.class:submitPanel} { background-color: #f0f0f0; border: 1px solid #d5d5d5; }' +
-	'.{plugin.class:submitHead} { max-width: 350px; float: left; margin: 10px; color: #515151; font-family: "Helvetica Neue", Helvetica, Arial, sans-serif; }' +
-	'.echo-sdk-ui .{plugin.class:submitPanel} > .{plugin.class:submitButton} { float:right; margin: 55px 10px 10px 0px; }' +
+	'.{class:postComposer} > a.dropdown-toggle {font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;}' +
+	'.{plugin.class:submitPanel} { background-color: #f0f0f0; border: 1px solid #d5d5d5; margin-bottom: 15px; }' +
+	'.{plugin.class:submitHead} { max-width: 350px; float: left; margin: 20px 0 20px 20px; color: #515151; font-family: "Helvetica Neue", Helvetica, Arial, sans-serif; }' +
+	'.echo-sdk-ui .{plugin.class:submitPanel} > .{plugin.class:submitButton} { float:right; margin: 65px 20px 10px 0px; }' +
 	'.{plugin.class:submitHeadBrand} { font-size: 21px; line-height: 35px; }' +
-	'.{plugin.class:submitHeadText} { font-size: 14px; line-height: 20px; }' +
-	'.{class:postComposer} { display: none; }';
+	'.{plugin.class:submitHeadText} { font-size: 14px; line-height: 20px; }';
 
 Echo.Plugin.create(plugin);
 
