0dd278ec10da8bc9150855b0fecef47440c187bf
Valeriy Shkiryak <lizard@aboutecho.com>
2014-08-13 13:10:45 +0400

    Use updated version of cloudapp-boilerplate

--- grunt/config/patch.js
+++ grunt/config/patch.js
@@ -4,11 +4,9 @@ module.exports = function(grunt) {
 		isotope: {
 			options: {
 				patcher: function(text) {
-					// TODO: allow ? to be at hte line beginning
-					// check requireOperatorBeforeLineBreak from jscs
-					var re = grunt.config('env.name') === 'development' ?
-						/window.jQuery/g :
-						/\w{1,2}\.jQuery/g;
+					var re = grunt.config('env.name') === 'development'
+						? /window.jQuery/g
+						: /\w{1,2}\.jQuery/g;
 					return text.replace(re, 'Echo.jQuery');
 				}
 			},
