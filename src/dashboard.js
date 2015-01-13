f7cff2b00aff6d8a3e5f3f1648410f18d6fdc845
Alexandr Evseev <evseev@aboutecho.com>
2015-01-13 13:07:21 +0300

    Comment added

--- src/dashboard.js
+++ src/dashboard.js
@@ -300,6 +300,8 @@ dashboard.init = function() {
 
 dashboard.methods.update = function() {
 	var args = Array.prototype.slice.call(arguments);
+	// merge both objects (nestedOverrides.original and args[0]) into new one
+	// to prevent overwriting nestedOverrides.original by args
 	args[0] = $.extend(true, {}, {
 		"config": this.get("nestedOverrides.original")
 	}, args[0]);
